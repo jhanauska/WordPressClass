2a0b55e3546df76148201a9b8a55505e06d11a01
James Hanauska <jameshanauska@gmail.com>
2014-10-30 19:33:40 -0700

    More styling

--- wp-content/themes/twentytwelve-child/archives-recipes.php
+++ wp-content/themes/twentytwelve-child/archives-recipes.php
@@ -3,8 +3,8 @@
 <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
 
 <?php endwhile; else : ?>
-  <h1><?php the_title() ?></h1>
-  <p><?php the_excerpt() ?></p>
+  <h1 id="inclass3a"><?php the_title() ?></h1>
+  <p id="inclass3b"><?php the_excerpt() ?></p>
   <p><?php _e( 'Sorry, no posts matched your criteria.' ); ?></p>
 <?php endif; ?>
 
