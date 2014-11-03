aa7045933fab03863e578d489f8afd85f85d3667
James Hanauska <jameshanauska@gmail.com>
2014-11-02 21:31:24 -0800

    Fixing some issues

--- wp-content/themes/twentyeleven-child/template-midterm-three-column.php
+++ wp-content/themes/twentyeleven-child/template-midterm-three-column.php
@@ -8,17 +8,25 @@ get_header(); ?>
 		<div id="container">
 			<div id="content" role="main">
                 <div class="column">
-                <p><?php the_field('title'); ?></p>
-                <p><?php the_field('content_field'); ?></p>
+                <p><?php the_field('title_col_1'); ?></p>
+                <p><?php the_field('content_field_col_1'); ?></p>
                 </div>
                 <div class="column">
-                <p><?php the_field('title'); ?></p>
-                <p><?php the_field('content_field'); ?></p>
+                <p><?php the_field('title_col_2'); ?></p>
+                <p><?php the_field('content_field_col_2'); ?></p>
                 </div>
                 <div class="column">
-                <p><?php the_field('title'); ?></p>
-                <p><?php the_field('content_field'); ?></p>
+                <p><?php the_field('title_col_3'); ?></p>
+                <p><?php the_field('content_field_col_3'); ?></p>
                 </div>
+  <?php if(have_posts()) : while (have_posts()) : the_post(); ?>
+
+    <h1 class="title"><?php the_title(); ?></h1>
+    <?php the_content(); ?>
+  
+  <?php endwhile; endif; ?>
+              
+
 			</div><!-- #content -->
 		</div><!-- #container -->
 
